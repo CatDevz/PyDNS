476769f8d0931a8b0bb669ea4c7c45cee0f18adb
Cody Quinn <codyquinn1122@gmail.com>
2021-02-23 23:37:37 -0600

    :sparkles: Added full header generation

--- src/dns.py
+++ src/dns.py
@@ -1,82 +1,151 @@
-from log import log, INFO, WARN, ERROR, CRITICAL, DEBUG
+from log import *
 import socket
+import json
+import glob
 import os
+import sys
 
 PORT = int(os.environ.get("PORT", 53))
 IP = os.environ.get("IP", "127.0.0.1")
 
 running = True
+zones = {}
+
+
+def loadZones():
+    """ Loads all zone files into JSON format """
+    jsonzone = {}
+    zonefiles = glob.glob("zones/*.zone")
+    for zonefile in zonefiles:
+        with open(zonefile) as file:
+            data = json.load(file)
+            jsonzone[data["$origin"]] = data
+    return jsonzone
+
+
+def getRecords(domainParts, rawQuestionType):
+    QUESTION_TYPES = {
+        b'\x00\x01': 'a',
+        b'\x00\x02': 'ns',
+        b'\x00\x03': 'md',
+        b'\x00\x04': 'mf',
+        b'\x00\x05': 'cname',
+        b'\x00\x06': 'soa',
+        b'\x00\x07': 'mb',
+        b'\x00\x08': 'mg',
+        b'\x00\x09': 'mr',
+        b'\x00\x0A': 'null',
+        b'\x00\x0B': 'wks',
+        b'\x00\x0C': 'ptr',
+        b'\x00\x0D': 'hinfo',
+        b'\x00\x0E': 'minfo',
+        b'\x00\x0F': 'mx',
+        b'\x00\x10': 'txt',
+    }
+
+    # Getting the question type in a readable form
+    questionType = QUESTION_TYPES[rawQuestionType]
+
+    # Getting the zone
+    zone_name = "{}.".format('.'.join(domainParts))
+    if not zone_name in zones:
+        return zone_name, None, questionType
+    zone = zones[zone_name]
+
+    # Getting the records for that question type and zone
+    records = zone[questionType]
+
+    return (zone_name, records, questionType)
+
+
+def getDomainQuestionFromBytes(data):
+    """ Decodes a domain & question from bytes """
+    domainParts = []
+
+    pointer = 0
+    nextByte = data[pointer]
+    while nextByte != b'\x00':
+        length = int(data[pointer])
+        pointer += 1
+
+        # Getting the string itself
+        domainParts.append("".join([data[i:i+1].decode('utf-8')
+                            for i in range(pointer, length + pointer)]))
+
+        # Adding stuff to the offset
+        pointer += length
+        nextByte = data[pointer:pointer+1]#.to_bytes(1, byteorder="big")
+    questionType = data[pointer+1:pointer+3]
+    return (domainParts, questionType)
+
+
+def getFlags(flags):
+    """ Returns flags to use in a DNS Response """
+    byte1 = bytes(flags[0:1])
+    byte2 = bytes(flags[1:2])
+
+    # Byte 1  ( 1XXXX100 )
+    QR = '1'
+    OPCODE = ''.join([str(ord(byte1)&(1<<bit)) for bit in range(1, 5)])
+    AA = '1'
+    TC = '0'    # Unsupported Feature ( xxx )
+    RD = '0'    # Unsupported Feature ( Recursion xxx )
+
+    # Byte 2  ( 00000000 )
+    RA = '0'    # Unsupported Feature ( Recursion Available )
+    Z = '000'   # Unused Bits ( "Reserved for future use" according to specification )
+    RC = '0000'
+
+    # Combining the bits, forming them into bytes, then returning them
+    return int(QR+OPCODE+AA+TC+RD, 2).to_bytes(1, byteorder='big') + \
+            int(RA+Z+RC, 2).to_bytes(1, byteorder='big')
 
 
 def buildResponse(data):
-    def getFlags(flags):
-        byte1 = bytes(flags[0:1])
-        byte2 = bytes(flags[1:2])
-
-        # Byte 1
-
-        QR = '1'
-
-        OPCODE = ''.join([str(ord(byte1)&(1<<bit)) for bit in range(1, 5)])
-
-        AA = '1'
-        TC = '0'    # Unsupported Feature ( xxx )
-        RD = '0'    # Unsupported Feature ( Recursion xxx )
-
-        # Byte 2
-        RA = '0'    # Unsupported Feature ( Recursion Available )
-        Z = '000'   # Unused Bits ( "Reserved for future use" according to specification )
-        RC = '0000'
-
-        # Combining the bits, forming them into bytes, then returning them
-        return int(QR+OPCODE+AA+TC+RD, 2).to_bytes(1, byteorder='big') + \
-                int(RA+Z+RC, 2).to_bytes(1, byteorder='big')
-
-    def getDomainFromBytes(domain):
-        def decodeString(b, offset):
-            length = int(b[:offset])
-            substring = "".join([b[i].to_bytes(1, byteorder='big').decode('utf-8')
-                                for i in range(offset, length+offset)])
-
-        print(decodeString(domain, 1))
-
-
-        # byteIndex = 0
-        # nextByte = domain[byteIndex]
-        # while (nextByte != b'\x00'):
-        #     domainStr +=
-        #
-        #     byteIndex += 1
-        #     nextByte = domain[byteIndex]
-        print(domain)
-
     res = b''
 
     try:
-        # Get the transaction ID
-        tid = data[:2]
-        tid_str = "".join([hex(byte)[2:].upper() for byte in tid])
-        log("Transaction ID: 0x{}".format(tid_str), DEBUG)
+        # Transaction ID
+        TID = data[:2]
 
-        # Get the flags
-        flags = getFlags(data[2:4])
-        log("Flags: {}".format(flags), DEBUG)
+        # Flags
+        FLAGS = getFlags(data[2:4])
 
         # Question Count
         QDCOUNT = b'\x00\x01'
 
         # Answer Count
-        getDomainFromBytes(data[12:])
+        domainParts, rawQuestionType = getDomainQuestionFromBytes(data[12:])
+        zone_name, records, questionType = getRecords(domainParts, rawQuestionType)
+        ANCOUNT = len(records).to_bytes(2, byteorder='big')
 
-        res = tid + flags
+        # Nameserver Count
+        NSCOUNT = b'\x00\x00'
+
+        # Additional Count
+        ARCOUNT = b'\x00\x00'
+
+        # Logging out some stuff
+        log("Zone Requested: {}".format(zone_name), DEBUG)
+        log("Question Type: {}".format(questionType), DEBUG)
+
+        # Creating the final DNS Header & Body
+        dnsheader = TID+FLAGS+QDCOUNT+ANCOUNT+NSCOUNT+ARCOUNT
+        dnsbody = b''  # TODO: Finish this tommorow :)
+
+        res = dnsheader + dnsbody
     except Exception as e:
         raise e
 
-    log("Response: {}".format(res), DEBUG)
+    log("Response Generated: {}".format(res), DEBUG)
     return res
 
 
 def main():
+    # Getting all the zones from zones folder
+    global zones
+    zones = loadZones()
+
     log("IP: {}".format(IP), DEBUG)
     log("PORT: {}".format(PORT), DEBUG)
 
